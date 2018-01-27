2c82f1c74d76599100178d84457c5c2bff7614f4
privod <privod.org@gmail.com>
2018-01-27 18:57:02 +0700

    Переход на конфигурирование из командной строки

--- project/backup.py
+++ project/backup.py
@@ -5,54 +5,25 @@ import os.path
 import argparse
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from config import Conf
-
-
-class BackupConf(Conf):
-    file_name = 'backup.cfg'
-    default = [
-        (None, 'path',     '.',                                'Путь к файлу/каталогу для архивирования'),
-        (None, 'archiver', 'C:\\Program Files\\7-Zip\\7z.exe', 'Путь к архиватору'),
-        (None, 'arc_name', 'name',                             'Имя файла архива'),
-        (None, 'gd_id', '', 'id архива в GDrive'),
-        (None, 'gd_parent_id', '', 'id родительского каталога в GDrive'),
-    ]
-    # path = '.'          # 'F:\\Games\\OpenXcom\\user\\xcom1'
-    # archiver = "C:\\Program Files\\7-Zip\\7z.exe"
-    # arc_name = 'name'   # 'xcom'
-
-def main_old():
-    conf = BackupConf()
-    if conf.file_created:
-        print('Создан новый конфигурационный файл.')
-        return
-
-    upload(
-        archiver=conf.get('archiver'),
-        arc_name=conf.get('arc_name'),
-        path=conf.get('path'),
-        gd_id=conf.get('gd_id'),
-        gd_parent_id=conf.get('gd_parent_id')
-    )
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Архивирует указанную папку и загружает в Google Drive')
-  parser.add_argument('-a', '--archiver',       help='Путь к архиватору 7z')
-  parser.add_argument('-n', '--arc-name',        help='Имя файла архива')
-  parser.add_argument('-p', '--path',           help='Путь к файлу/каталогу для архивирования')
-  parser.add_argument('-gd', '--gdrive-dir',    help='id архива в Google Drive')
-  parser.add_argument('-gp', '--gdrive-parent', help='id родительского каталога в Google Drive')
+    parser = argparse.ArgumentParser(description='Архивирует указанную папку и загружает в Google Drive')
+    parser.add_argument('-a', '--archiver',       help='Путь к архиватору 7z')
+    parser.add_argument('-n', '--arc-name',        help='Имя файла архива')
+    parser.add_argument('-p', '--path',           help='Путь к файлу/каталогу для архивирования')
+    parser.add_argument('-gd', '--gdrive-dir',    help='id архива в Google Drive')
+    parser.add_argument('-gp', '--gdrive-parent', help='id родительского каталога в Google Drive')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  upload(
-      args.archiver,
-      args.arc_name,
-      args.path,
-      args.gdrive_dir,
-      args.gdrive_parent
-  )
+    upload(
+        args.archiver,
+        args.arc_name,
+        args.path,
+        args.gdrive_dir,
+        args.gdrive_parent,
+    )
 
 
 def upload(archiver, arc_name, path, gd_id, gd_parent_id):
--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
 
     install_requires=[
         'PyDrive',
-        'config'        # Устанавливается локально (./lib/config-1.0.tar.gz) смотри ./cmd/install.cmd
     ],
     entry_points={
         'console_scripts': [
